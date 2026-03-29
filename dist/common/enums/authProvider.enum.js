0c043868ab24d0aea8050eece01ad75a43c2d7df
Ashik Muhammed <ashikmuhammed@Ashiks-MacBook-Pro.local>
2026-03-29 10:14:54 +0530

    fix: correct enum file casing

---
+++ dist/common/enums/authProvider.enum.js
@@ -0,0 +1,8 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.AuthProvider = void 0;
+var AuthProvider;
+(function (AuthProvider) {
+    AuthProvider["LOCAL"] = "LOCAL";
+    AuthProvider["GOOGLE"] = "GOOGLE";
+})(AuthProvider || (exports.AuthProvider = AuthProvider = {}));
